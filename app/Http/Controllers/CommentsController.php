5feab7d133c6fa6b46b3ae076415c2fd229aeeae
Amir <amir.the.h.95@gmail.com>
2021-11-05 21:44:02 +0330

    Simple backend

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Comments\StoreRequest;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+    /**
+     * List all comments with nested children
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $comments = Comment::query()
+            ->whereNull('parent_id') // only get parent comments
+            ->with(['children']) // then retreive childrens as well
+            ->latest() // order comments by the lastest
+            ->get();
+
+        return response()->json($comments);
+    }
+
+    /**
+     * Store new comment or new reply to another comment
+     *
+     * @param  StoreRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRequest $request)
+    {
+        // default depth is 1
+        $depth = 1;
+        $parent_id = $request->get('parrent_id');
+        if (!is_null($parent_id)) {
+            // no need to sanity check for existance of the comment.
+            // request validation already checked.
+            $parent = Comment::find($parent_id);
+            $depth = $parent->depth + 1;
+            // reject replying on max layer
+            if ($depth > Comment::MAX_LAYER) {
+                return response()->json(['error' => 'It is not possible to reply much deeper.'], 400);
+            }
+        }
+
+        // prepare the object
+        $comment = new Comment;
+        $comment->name = $request->get('name');
+        $comment->body = $request->get('body');
+        $comment->parent_id = $parent_id;
+        $comment->depth = $depth;
+        // store it
+        $comment->save();
+
+        return response()->json(['message' => 'Your comment has been stored.'], 201);
+    }
+}
---
+++ database/migrations/2021_11_05_124331_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Comment;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Comment::class, 'parent_id')->nullable();
+            $table->string('name'); // user's name
+            $table->text('body'); // comment's contents
+            $table->unsignedTinyInteger('depth')->default(1); // hack for preventing max layer leak
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
